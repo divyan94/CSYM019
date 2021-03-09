d8c4037dbaac13a9e781dde3c404059166156957
Naga divya Peddi <divyan@Nagas-MacBook-Pro.local>
2021-03-09 20:28:58 +0000

    Weekly Sample Notes

---
+++ Week-4/js/js1.js
@@ -0,0 +1,18 @@
+/*alert("Loadeddd!!!!!!!!!!!!");
+
+var v1= 23;
+var v2 = 45;
+alert(v1+v2);
+
+var v3= "BTS"
+alert("Divya loves" + " " + v3);
+
+function myLove()
+{
+	alert("Divya Loves V");
+}
+myLove();
+*/
+
+var pid = document.getElementById('p1');
+alert("Id is" + " " + pid);
